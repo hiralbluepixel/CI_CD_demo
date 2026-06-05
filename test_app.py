037109be7c8b4f20e6997346b04926e36c293e5e
hiralbluepixel <hiral.bluepixel@gmail.com>
2026-06-05 18:47:45 +0530

    Initial commit

---
+++ test_app.py
@@ -0,0 +1,10 @@
+from app import app
+import pytest
+
+def test_hello_world():
+    client = app.test_client()
+
+    response = client.get('/')
+
+    assert response.status_code == 200
+    assert response.data.decode('utf-8') == "Hello World"
